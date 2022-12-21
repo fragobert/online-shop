07db757754be1cb14a3414534b720cbf9b2ca1c6
Fragobert <staflerflorian@gmail.com>
2022-12-21 09:47:39 +0100

    Initial commit

---
+++ src/Shop.java
@@ -0,0 +1,7 @@
+public class Shop {
+
+	public static void main(String[] args) {
+		Verkaufer v1 = new Verkaufer(); 
+	}
+
+}
---
+++ src/Verkaufer.java
@@ -0,0 +1,19 @@
+public class Verkaufer {
+	private String username = "Hannes", password = "Kennwort0"; 
+	public Verkaufer(String username, String password){
+		this.username = username; 
+		this.password = password; 
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
